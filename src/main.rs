234903aa41587354d1ab86cbc8c358fcf233603f
DocJade Desktop <ytdocjade@gmail.com>
2023-09-01 00:31:47 -0600

    Get token from CLI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+// DocJade 2023
+
+// Import the CLI argument parser
+use clap::Parser;
+
+// Set up command line arguments
+#[derive(Parser, Debug)]
+struct Args {
+    // Youtube API token
+    // Required.
+    #[arg(short, long, required = true)]
+    api_token: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    let api_token = args.api_token;
 }
