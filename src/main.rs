89036d79e069a4f18aea37eeb4e4a2cc804095a0
DocJade Desktop <ytdocjade@gmail.com>
2023-09-01 02:54:21 -0600

    Working rick-roll curl!

--- src/main.rs
+++ src/main.rs
@@ -14,38 +14,67 @@
 // Import the CLI argument parser
 use clap::Parser;
 // curl library
-use curl::easy::Easy;
+use curl::easy::{Easy, List};
 
 // Set up command line arguments
 #[derive(Parser, Debug)]
 struct Args {
     // Youtube API token
     // Required.
-    #[arg(short, long, required = true)]
-    api_token: String,
+    #[arg(long, required = true)]
+    api_key: String
 }
+
+// Store this bit of the youtube url to save space
+const API_URL: &str = "https://youtube.googleapis.com/youtube/v3/";
 
 fn main() {
     // Grab the Token from CLI
     let args = Args::parse();
-    let api_token = args.api_token;
+    let api_key: &str = &args.api_key;
 
     // Test the token.
-    print!("{:?}", c_get(api_token.to_string()));
+    match test_token(api_key) {
+        Some(test_fail) => panic!("Bad key or token: {:?}", test_fail),
+        None => ()
+    }
+
     //token_test = todo!()
+}
+
+#[derive(Debug)]
+enum TestFail {
+    CurlFailure(CurlFail),
+    BadToken
+}
+
+fn test_token(key: &str) -> Option<TestFail> {
+    // Build the test URL
+    let function: String = "search?part=snippet".to_string();
+    let max_results: String = "&maxResults=1".to_string();
+    let search_term: String = "&q=Never%20gonna%20give%20you%20up".to_string();
+    let the_key: String = format!("&key={}",&key);
+    let query: String = API_URL.to_owned() + &function + &max_results + &search_term + &the_key;
+    let result = c_get(&query);
+    match result {
+        Ok(_) => (),
+        Err(e) => return Some(TestFail::CurlFailure(e)),
+    };
+    print!("{:?}",result);
+    panic!("test")
 }
 
 // Make some easier functions for Curl
 
 #[derive(Debug)]
 enum CurlFail {
-    AllGood,
-    SomethingBroke,
+    SomethingBroke(String),
     BadURL,
     DataIssue,
+    HeaderIssue
 }
 
-fn c_get(input: String) -> Result<String, CurlFail> {
+fn c_get(input: &str) -> Result<String, CurlFail> {
     use std::sync::{Arc, Mutex};
     // create an easy from CURL
     let mut curl = Easy::new();
@@ -57,6 +86,15 @@ fn c_get(input: String) -> Result<String, CurlFail> {
     match curl.url(&input) {
         Ok(_) => (),
         Err(_) => return Err(CurlFail::BadURL),
+    }
+
+    // Set headers
+    let mut headers = List::new();
+    headers.append("Accept: application/json");
+    
+    match curl.http_headers(headers) {
+        Ok(_) => (),
+        Err(_) => return Err(CurlFail::HeaderIssue),
     }
 
     // Clone the Arc for the closure.
@@ -75,7 +113,7 @@ fn c_get(input: String) -> Result<String, CurlFail> {
 
     match curl.perform() {
         Ok(_) => (),
-        Err(_) => return Err(CurlFail::SomethingBroke),
+        Err(e) => return Err(CurlFail::SomethingBroke(e.to_string())),
     }
 
     // Convert the Vec<u8> to a String.
@@ -83,6 +121,6 @@ fn c_get(input: String) -> Result<String, CurlFail> {
     Ok(response_string)
 }
 
-fn c_post(input: String) -> Result<String, CurlFail> {
-    todo!()
-}
+//fn c_post(input: String) -> Result<String, CurlFail> {
+//    todo!()
+//}
